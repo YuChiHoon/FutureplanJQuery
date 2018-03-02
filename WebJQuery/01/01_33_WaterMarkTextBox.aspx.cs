6a5f0e828e6e51f1747a108e7cd6e8ce0c86218e
YuChiHoon <penych1015@gmail.com>
2018-03-02 23:32:14 +0900

    2018_03_02

---
+++ WebJQuery/01/01_33_WaterMarkTextBox.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _01_01_33_WaterMarkTextBox : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Write("입력한 아이디 : " + Request["txtUserID"]);
+    }
+}
